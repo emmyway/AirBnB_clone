ff1a205be3421fac18e4652f0255328ea387f0ed
Waheed Khaled <waheedkhaled95@gmail.com>
2024-02-12 01:31:52 +0200

    added models

---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Contains Amenity class."""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Represent an amenity.
+
+    Attributes:
+        name (str): The name of the amenity.
+    """
+
+    name = ""
